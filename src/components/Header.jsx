c1c6ffa130656bd89a7a634407c4c5962f4b76de
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-05-28 15:03:19 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,118 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+const navigation = [
+  { name: 'Free Readings', href: '#' },
+  { name: 'Paid Readings', href: '#' },
+  { name: 'Contact Us', href: '#' },
+];
+
+export function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50 bg-gradient-to-b from-slate-950/50">
+      <nav
+        className="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex flex-1">
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-medium leading-6 transition-all duration-150 ease-in-out text-white/70 hover:text-white"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="w-6 h-6" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+        <Link href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Mystic Tarot Chat</span>
+          <Image
+            className="w-auto h-32"
+            width={376}
+            height={382}
+            src="https://res.cloudinary.com/dt3k2apqd/image/upload/v1685267539/Mystic%20Tarot%20Chat/logo_light_ayh3oe.png"
+            alt="Mystic Tarot Chat Logo"
+          />
+        </Link>
+        <div className="flex justify-end flex-1">
+          <Link
+            href="#"
+            className="text-sm font-semibold leading-6 text-white/70"
+          >
+            Start your journey <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 left-0 z-10 w-full px-6 py-6 overflow-y-auto bg-slate-950">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-1">
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="w-6 h-6" aria-hidden="true" />
+              </button>
+            </div>
+            <Link href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Mystic Tarot Chat</span>
+              <Image
+                className="w-auto h-32"
+                width={376}
+                height={382}
+                src="https://res.cloudinary.com/dt3k2apqd/image/upload/v1685267539/Mystic%20Tarot%20Chat/logo_light_ayh3oe.png"
+                alt="Mystic Tarot Chat Logo"
+              />
+            </Link>
+            <div className="flex justify-end flex-1">
+              <Link
+                href="#"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                Start your journey <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
+          </div>
+          <div className="mt-6 space-y-2">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 transition duration-150 ease-in-out rounded-lg text-white/70 hover:bg-slate-900"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/dt3k2apqd/image/upload/v1685273690/Mystic%20Tarot%20Chat/favicon_qptpoh.svg"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/asg4jef.css"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
