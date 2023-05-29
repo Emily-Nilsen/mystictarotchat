869438341eeed9b94fca7f1e2dda4da16e9c3195
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-05-29 07:16:57 +0200

    Add meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,38 @@
 import Image from 'next/image';
+import Head from 'next/head';
 import { HomeHero } from '@/components/HomeHero';
 
 export default function Home() {
   return (
-    <main className="relative w-full min-h-screen">
-      <div className="absolute inset-0 bg-slate-950">
-        <Image
-          priority
-          fill
-          src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Mystic%20Tarot%20Chat/tarot_cards_backdrop_vcf3oo.webp"
-          alt="Tarot cards collage"
-          className="object-cover"
+    <>
+      <Head>
+        <title>
+          Mystic Tarot Chat - Online Tarot Card Readings for Insight and
+          Guidance
+        </title>
+        <meta
+          name="description"
+          content="Unveil your future with Mystic Tarot Chat's online tarot card readings. Gain guidance now!"
         />
-        <div className="absolute inset-0 bg-slate-950/90 mix-blend-multiply" />
-      </div>
-      <HomeHero />
-    </main>
+        <link
+          rel="canonical"
+          href="https://www.mystictarotchat.com/"
+          key="canonical"
+        />
+      </Head>
+      <main className="relative w-full min-h-screen">
+        <div className="absolute inset-0 bg-slate-950">
+          <Image
+            priority
+            fill
+            src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Mystic%20Tarot%20Chat/tarot_cards_backdrop_vcf3oo.webp"
+            alt="Tarot cards collage"
+            className="object-cover"
+          />
+          <div className="absolute inset-0 bg-slate-950/90 mix-blend-multiply" />
+        </div>
+        <HomeHero />
+      </main>
+    </>
   );
 }
-
-// Mystic Tarot Chat - Online Tarot Card Readings for Insight and Guidance
-// Unveil your future with Mystic Tarot Chat's online tarot card readings. Gain guidance now!
